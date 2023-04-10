8229b240482ae0a53b4d9434623cb55107f51a96
yeyejn <yeounshi@gmail.com>
2023-04-10 22:59:10 +0900

    예제 도메인 모델과 동작확인(순수 JPA)

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,30 +1,47 @@
 package study.datajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrimaryKeyJoinColumn;
-import lombok.Generated;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+
+import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","username","age"}) //team은적으면안돼 무한루프뜸
 public class Member {
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-
     private String username;
+    private  int age;
 
-    protected Member() {
-    }
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+
 
     public Member(String username) {
         this.username = username;
     }
 
+    public Member(String name, int age, Team team) {
+        this.username = name;
+        this.age = age;
+        if(team!=null){
+            changeTeam(team);
+        }
+    }
+
     public void changeUsername(String username){
         this.username=username;
+    }
+
+    //연관관계 세팅
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
 
